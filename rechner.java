36b9e710018dd0451e19595c473e0e56f079b6a3
DevBaka <devbaka@gmx.net>
2016-08-12 00:19:44 +0100

    working for binär in dez
    binär in dezimal rechnung angefangen und etwas ordnung geschaffen

--- rechner.java
+++ rechner.java
@@ -6,14 +6,17 @@ import java.io.InputStream;
 
 public class rechner {
 	public static void main(String[] args){
-		String eingabe = "";
+		String ozahlensystem = "";
+		String izahlensystem = "";
 		String zahl = "";
 		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 		
-		while(!eingabe.contains("quit") && !eingabe.contains("exit") && !zahl.contains("quit") && !zahl.contains("exit")){
+		while(!ozahlensystem.contains("quit") && !ozahlensystem.contains("exit") && !zahl.contains("quit") && !zahl.contains("exit") && !izahlensystem.contains("quit") && !izahlensystem.contains("exit")){
 			try {
-				System.out.println("Zahlensystem|stellenwert angeben: ");
-				eingabe = console.readLine();
+				System.out.println("Welches Zahlensystem soll ausgegeben werden? ");
+				ozahlensystem = console.readLine();
+				System.out.println("Welches Zahlensystem wurde eingegeben? ");
+				izahlensystem = console.readLine();
 				System.out.println("Zahl angeben: ");
 				zahl = console.readLine();
 			} 
@@ -21,32 +24,52 @@ public class rechner {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println("Ergebnis: " +  rechner(Integer.parseInt(eingabe), Integer.parseInt(zahl)));
+			System.out.println("Ergebnis: " +  rechner(Integer.parseInt(ozahlensystem), Integer.parseInt(zahl), Integer.parseInt(izahlensystem)));
 			
 		}
 		
 	}
 	
-	public static String rechner(int zahlensystem, int zahl){
+	public static String rechner(int ozahlensystem, int zahl, int izahlensystem){
 		
-		long ergebnis = zahl;
-		long ergebnis2 = 0;
-		boolean done = false;
-		String endsumme = "";
-		
-		while(done == false){
-			ergebnis2 = ergebnis % zahlensystem;
-			ergebnis = ergebnis / zahlensystem;
-			endsumme = endsumme + ergebnis2;
+		String endergebnis = "";
+		//dez in binär
+		if(ozahlensystem == 2 && izahlensystem == 10){
+			long ergebnis = zahl;
+			long ergebnis2 = 0;
+			boolean done = false;
+			String endsumme = "";
 			
-			if(ergebnis / zahlensystem == 0.0){
-				ergebnis2 = ergebnis % zahlensystem;
+			while(done == false){
+				ergebnis2 = ergebnis % ozahlensystem;
+				ergebnis = ergebnis / ozahlensystem;
 				endsumme = endsumme + ergebnis2;
-				done = true;
+				
+				if(ergebnis / ozahlensystem == 0.0){
+					ergebnis2 = ergebnis % ozahlensystem;
+					endsumme = endsumme + ergebnis2;
+					done = true;
+				}
+				
 			}
-			
+			endergebnis =  stringdreher(endsumme);
 		}
-		return stringdreher(endsumme);
+		
+		//binär in dez
+		if(ozahlensystem == 10 && izahlensystem == 2){
+			String istring = Integer.toString(izahlensystem);
+			int zähler = 1;
+			int ergebnis = 0;
+			for(int i=istring.length() - 1; i >= 0; i = i -1){
+				zähler = zähler * 2;
+				char sposition = istring.charAt(i);
+				ergebnis = ergebnis + ((int)sposition * zähler);
+			}
+			endergebnis = Integer.toString(ergebnis);
+		}
+		
+		return endergebnis;
+		
 	}
 	
 	public static String stringdreher(String s){
