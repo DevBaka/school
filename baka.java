a5e7e53b6c6ffcd5f7ba4b392c824dfc9362c38b
DevBaka <devbaka@gmx.net>
2016-08-11 23:49:23 +0100

    Dez in Binär
    Dezimal in Binär ist soweit möglich.

--- baka.java
+++ baka.java
@@ -1,41 +1,60 @@
+package rechner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.InputStream;
 
-/*
- * Zahlensysteme konvertieren.
- * Benutzer soll angeben, welches Zahlensystem (2,8,10,15 etc...)
- * Benutzer soll Zahl angeben
- * Benutzer soll Zahl im anderen Zahlensystem zurückerhalten
- */
-
-
-public class baka{
+public class rechner {
 	public static void main(String[] args){
 		String eingabe = "";
 		String zahl = "";
 		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 		
-		while(!eingabe.contains("quit")){
+		while(!eingabe.contains("quit") && !eingabe.contains("exit") && !zahl.contains("quit") && !zahl.contains("exit")){
 			try {
 				System.out.println("Zahlensystem|stellenwert angeben: ");
 				eingabe = console.readLine();
-				zahl = console.readLine()
-			} catch (IOException e) {
+				System.out.println("Zahl angeben: ");
+				zahl = console.readLine();
+			} 
+			catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			rechner(eingabe, zahl);
+			System.out.println("Ergebnis: " +  rechner(Integer.parseInt(eingabe), Integer.parseInt(zahl)));
+			
 		}
 		
 	}
 	
-	public static void rechner(String zahlensystem, String zahl){
-		System.out.println(zahlensystem):
-		for(int i=0; i < zahl.length(); i++){
+	public static String rechner(int zahlensystem, int zahl){
+		
+		long ergebnis = zahl;
+		long ergebnis2 = 0;
+		boolean done = false;
+		String endsumme = "";
+		
+		while(done == false){
+			ergebnis2 = ergebnis % zahlensystem;
+			ergebnis = ergebnis / zahlensystem;
+			endsumme = endsumme + ergebnis2;
+			
+			if(ergebnis / zahlensystem == 0.0){
+				ergebnis2 = ergebnis % zahlensystem;
+				endsumme = endsumme + ergebnis2;
+				done = true;
+			}
 			
 		}
-		
+		return stringdreher(endsumme);
+	}
+	
+	public static String stringdreher(String s){
+		String endergebnis = "";
+		for(int i=s.length() -1; i >= 0; i = i - 1){
+			char sposition = s.charAt(i);
+			endergebnis = endergebnis + sposition;
+		}
+		return endergebnis;
 	}
 }
